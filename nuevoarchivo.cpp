a39223d140faf40bef7e34b867f33e8615069bbb
Fidel85coder <fidelignacio2005@gmail.com>
2024-09-16 10:03:07 -0600

    Pruebo subir archivo

---
+++ nuevoarchivo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Declarar variables para los números y el resultado
+    double num1, num2, suma;
+
+    // Solicitar al usuario que ingrese dos números
+    cout << "Ingrese el primer número: ";
+    cin >> num1;
+
+    cout << "Ingrese el segundo número: ";
+    cin >> num2;
+
+    // Realizar la suma
+    suma = num1 + num2;
+
+    // Mostrar el resultado
+    cout << "La suma de " << num1 << " y " << num2 << " es: " << suma << endl;
+
+    return 0;
+}
+
